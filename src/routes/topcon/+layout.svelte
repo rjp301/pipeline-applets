59dad4cbf3f4be5dc39d9cecffe78316e8c0a492
Riley Paul <rileypaul96@gmail.com>
2023-05-02 09:14:18 -0600

    use chips to represent run parameters

--- src/routes/topcon/+layout.svelte
+++ src/routes/topcon/+layout.svelte
@@ -1,47 +1,34 @@
 <script lang="ts">
-  import { page } from "$app/stores";
-  import {
-    AppShell,
-    AppRail,
-    AppRailTile,
-    AppBar,
-  } from "@skeletonlabs/skeleton";
-  import type { LayoutData } from "./$types";
-  export let data: LayoutData;
+	import { page } from '$app/stores';
+	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+	import type { LayoutData } from './$types';
+	export let data: LayoutData;
 
-  $: classesActive = (href: string) =>
-    href === $page.url.pathname ? "!bg-primary-500" : "";
+	$: classesActive = (href: string) => (href === $page.url.pathname ? '!bg-primary-500' : '');
 </script>
 
 <AppShell>
-  <svelte:fragment slot="header">
-    <AppBar><h1>Topcon Ditch Volume Calculation</h1></AppBar>
-  </svelte:fragment>
-  <svelte:fragment slot="sidebarLeft">
-    <nav class="list-nav p-2">
-      <a
-        href="/topcon"
-        class={"btn variant-outline-primary mb-4"}
-        class:bg-primary-500={$page.url.pathname === "/topcon"}
-      >
-        <span class="badge bg-primary-500">+</span>
-        <span class="flex-auto">New Calculation</span></a
-      >
-      <span class="font-bold px-2">Previous Calculations</span>
-      <ul>
-        {#each data.topconRuns as run}
-          <li>
-            <a
-              href={`/topcon/${run.id}`}
-              class={classesActive(`/topcon/${run.id}`)}
-            >
-              <span class="badge bg-primary-500">→</span>
-              <span class="flex-auto">{run.KP_rng}</span>
-            </a>
-          </li>
-        {/each}
-      </ul>
-    </nav>
-  </svelte:fragment>
-  <slot />
+	<svelte:fragment slot="header">
+		<AppBar><h1>TOPCON Ditch Volume Calculation</h1></AppBar>
+	</svelte:fragment>
+	<svelte:fragment slot="sidebarLeft">
+		<nav class="list-nav p-2">
+			<a href="/topcon" class={'variant-outline-primary mb-4 ' + classesActive('/topcon')}>
+				<span class="badge bg-primary-500">+</span>
+				<span class="flex-auto">New Calculation</span></a
+			>
+			<span class="font-bold px-2">Previous Calculations</span>
+			<ul>
+				{#each data.topconRuns as run}
+					<li>
+						<a href={`/topcon/${run.id}`} class={classesActive(`/topcon/${run.id}`)}>
+							<span class="badge bg-primary-500">→</span>
+							<span class="flex-auto">{run.KP_rng}</span>
+						</a>
+					</li>
+				{/each}
+			</ul>
+		</nav>
+	</svelte:fragment>
+	<slot />
 </AppShell>
--- src/routes/topcon/[id]/+page.svelte
+++ src/routes/topcon/[id]/+page.svelte
@@ -1,79 +1,65 @@
 <script lang="ts">
-  import type { PageData } from "./$types";
-  import Table from "$lib/components/Table.svelte";
+	import type { PageData } from './$types';
+	import Table from '$lib/components/Table.svelte';
 
-  export let data: PageData;
+	export let data: PageData;
 
-  const data_pts_head = [
-    "num",
-    "x",
-    "y",
-    "z",
-    "desc",
-    "chainage",
-    "slope",
-    "width_bot",
-    "width_top",
-    "area",
-  ];
+	const data_pts_head = [
+		'num',
+		'x',
+		'y',
+		'z',
+		'desc',
+		'chainage',
+		'slope',
+		'width_bot',
+		'width_top',
+		'area'
+	];
 
-  const data_rng_head = [
-    "KP_beg",
-    "KP_end",
-    "area_beg",
-    "area_end",
-    "area_avg",
-    "length",
-    "volume",
-  ];
-
-  const countDecimals = (num: number) => {
-    if (Math.floor(num) === num) return 0;
-    return num.toString().split(".")[1].length;
-  };
-
-  const displayNumber = (num: number, cutoff: number) => {
-    if (countDecimals(num) <= cutoff) return num.toString();
-    return num.toFixed(cutoff);
-  };
-
-  const displayValue = (value: number | string | null) => {
-    if (value === null) return "";
-    if (typeof value === "string") return value;
-    return displayNumber(value, 2);
-  };
-
+	const data_rng_head = [
+		'KP_beg',
+		'KP_end',
+		'area_beg',
+		'area_end',
+		'area_avg',
+		'length',
+		'volume'
+	];
 </script>
 
-<main class="p-4">
-  <div class="flex justify-between items-center">
-    <div>
-      <h2>{data.topconRun.KP_rng}</h2>
-      <span>{new Date(data.topconRun.createdAt)}</span>
-    </div>
+<main class="p-4 flex flex-col gap-4">
+	<div class="flex justify-between items-center">
+		<div>
+			<h2>{data.topconRun.KP_rng}</h2>
+			<small>{new Date(data.topconRun.createdAt)}</small>
+		</div>
 
-    <div class="flex gap-2">
-      <form method="POST" action="?/delete">
-        <button class="btn variant-filled"> Delete </button>
-      </form>
-      <a
-        href={data.topconRunDownloadUrl}
-        class="btn variant-filled-primary"
-        download
-      >
-        Download Excel
-      </a>
-    </div>
-  </div>
-  <br />
-  <div class="text-lg font-bold text-red-500">
-    Total Volume: {Math.round(data.topconRun.total_volume)} m<sup>3</sup>
-  </div>
-  <div>Width Bottom: {data.topconRun.width_bot}m</div>
-  <div>Slope: {data.topconRun.slope}:1</div>
-  <h3>Point Data</h3>
-  <Table columns={data_pts_head} data={data.topconRun.data_pts} />
+		<div class="flex gap-2">
+			<form method="POST" action="?/delete">
+				<button class="btn variant-filled"> Delete </button>
+			</form>
+			<a href={data.topconRunDownloadUrl} class="btn variant-filled-primary" download>
+				Download Excel
+			</a>
+		</div>
+	</div>
 
-  <h3>Range Data</h3>
-  <Table columns={data_rng_head} data={data.topconRun.data_rng} />
+	<div>
+		<span class="chip variant-filled-primary"
+			>Total {Math.round(data.topconRun.total_volume)} m<sup>3</sup></span
+		>
+		<span class="chip variant-outline">Width Bottom {data.topconRun.width_bot} m</span>
+		<span class="chip variant-outline">Ditch Slope {data.topconRun.slope}:1</span>
+	</div>
+
+	<div>
+		<h3 class="mb-2">Point Data</h3>
+		<Table columns={data_pts_head} data={data.topconRun.data_pts} />
+	</div>
+
+	<div>
+		<h3 class="mb-2">Range Data</h3>
+		<Table columns={data_rng_head} data={data.topconRun.data_rng} />
+	</div>
 </main>
