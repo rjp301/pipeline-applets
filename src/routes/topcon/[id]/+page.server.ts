13168850580b62c1688319c91f912118efc98e86
Riley Paul <rileypaul96@gmail.com>
2023-06-05 07:23:22 -0700

    update bug in delete function

--- src/routes/topcon/[id]/+page.server.ts
+++ src/routes/topcon/[id]/+page.server.ts
@@ -1,13 +1,13 @@
 import type { PageServerLoad } from './$types';
 
-import { redirect, type Actions} from '@sveltejs/kit';
+import { redirect, type Actions } from '@sveltejs/kit';
 
 export const load: PageServerLoad = async ({ params, fetch }) => {
 	return { topconRun: fetch(`/topcon/${params.id}`).then((res) => res.json()) };
 };
 
 export const actions: Actions = {
-	deleteRun: async ({ params }) => {
+	deleteRun: async ({ params, fetch }) => {
 		await fetch(`/topcon/${params.id}`, { method: 'DELETE' });
 		throw redirect(303, '/topcon');
 	}
